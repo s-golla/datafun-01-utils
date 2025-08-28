8284eee95b436b93c6558d2a767ce80bc42773cf
s-golla <saratchandragolla@gmail.com>
2025-08-28 21:54:57 +0530

    completed todo tasks

--- utils_sgolla.py
+++ utils_sgolla.py
@@ -1,5 +1,5 @@
 """
-File: utils_case.py
+File: utils_sgolla.py
 
 Purpose: Reusable header/tagline module for analytics projects.
 
@@ -10,11 +10,8 @@ A short, first-week module to demonstrate key skills:
 - expose a function named get_tagline() that can be imported into other modules
 - run this file as a script via main() using the if __name__ == '__main__' pattern
 
-Author: Denise Case
+Author: Saratchandra Golla
 
-TODO: Replace 'lastname' in the filename with your own last (or unique) name.
-TODO: Update the file name and author in this opening docstring.
-TODO: Remove each TODO line AFTER you successfully complete the item.
 """
 
 #####################################
@@ -58,26 +55,23 @@ logger.info("Logger loaded.")
 # ----------------------------------
 is_accepting_clients: bool = True
 offers_remote_workshops: bool = True
-
-# TODO: Declare and initialize a new boolean variable (e.g. `is_hiring` and set it to False)
+is_hiring: bool = False
 
 # ----------------------------------
 # Define Integer variables
 # ----------------------------------
 current_year: int = 2025
 year_started: int = 2020
-
-# TODO: Declare and initialize a new integer variable (e.g. `number_of_employees` and set it to 25)
+number_of_employees: int = 25
 
 # ----------------------------------
 # Define String variables
 # ----------------------------------
-author: str = "Denise Case"  # TODO: change to your name
-organization: str = "Stellar Analytics"  # TODO: change the org name
-motto: str = "Clear. Useful. On time."  # TODO: change the motto
+author: str = "Saratchandra Golla"
+organization: str = "Sgolla Analytics"
+motto: str = "Clear. Useful. On time. Learn."
 
-# TODO: Declare and initialize a new string variable (e.g. `location` and set it to your city and state)
-
+location: str = "O'Fallon, Missouri"
 # ----------------------------------
 # Define List variables
 # ----------------------------------
@@ -88,8 +82,7 @@ services: list[str] = ["Data Analysis", "Machine Learning", "Business Intelligen
 satisfaction_scores: list[float] = [4.8, 4.6, 4.9, 5.0, 4.7]
 
 
-# TODO: Declare and initialize a new list variable (e.g. `office_locations` and set it to 3-5 cities where your org has offices)
-# TODO: Wrap each string in quotes and separate each item with a comma.
+office_locations: list[str] = ["St. Louis, MO", "Chicago, IL", "New York, NY"]
 
 # ----------------------------------
 # Use built-in Python operators (such as - + * /)
@@ -101,8 +94,7 @@ max_score: float = max(satisfaction_scores)
 count_of_services: int = len(services)
 count_of_scores: int = len(satisfaction_scores)
 
-# TODO: Declare and initialize a new calculated variable (e.g. `count_of_locations` that uses the len() function on your list of office locations)
-
+count_of_locations: int = len(office_locations)
 
 # ----------------------------------
 # Use the built-in statistics module functions (such as mean, stdev, etc.)
@@ -113,25 +105,29 @@ stdev_score: float = statistics.stdev(satisfaction_scores)
 # ----------------------------------
 # Compose a reusable formatted string (f-string) using triple quotes
 # ----------------------------------
+
 byline: str = f"""
 **********************************************************
 {organization} — Project Header
 **********************************************************
 Author:                     {author}
 Motto:                      {motto}
+Location:                   {location}
 Years Active:               {years_active}
 Accepting New Clients?:     {is_accepting_clients}
 Remote Workshops?:          {offers_remote_workshops}
+Hiring?:                    {is_hiring}
+Number of Employees:        {number_of_employees}
 Services:                   {services}
+Office Locations:           {office_locations}
+Count of Locations:         {count_of_locations}
 Client Satisfaction Scores: {satisfaction_scores}
 Minimum Satisfaction Score: {min_score}
 Maximum Satisfaction Score: {max_score}
 Mean Satisfaction Score:    {mean_score:.2f}
-     Standard Deviation:    {stdev_score:.2f}
+    Standard Deviation:    {stdev_score:.2f}
 **********************************************************
 """
-
-# TODO: Modify the byline f-string to display your new variables as well. Use curly braces {} to embed each variable.
 
 #####################################
 # Define Global Functions
@@ -170,8 +166,7 @@ def main() -> None:
     loguru.logger.info("Byline:\n" + get_byline())
 
     try:
-        # TODO: Uncomment next line if you want audio feedback (use CTRL+C to stop)
-        # read_byline_aloud()
+        read_byline_aloud()
         pass
     except KeyboardInterrupt:
         logger.info("Speech interrupted by user (Ctrl+C).")
